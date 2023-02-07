73f6e7932a0d0f765615525c448aa3cba1155a5e
= <r.netofreire45@gmail.com>
2023-02-07 16:35:01 -0300

    cnab em andamento

--- FileCNAB/models.py
+++ FileCNAB/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-from django import forms
+
+
+class FileCNAB(models.Model):
+  tipo = models.IntegerField(max_length=1)
+  data = models.TextField(max_length=8)
+  valor = models.IntegerField(max_length=10)
+  CPF = models.IntegerField(max_length=11)
+  cartão = models.IntegerField(max_length=12)
+  hora = models.IntegerField(max_length=6)
+  dono = models.TextField(max_length=14)
+  loja = models.TextField(max_length=19)
 
--- FileCNAB/views.py
+++ FileCNAB/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 def upload(request):
+  uploaded_file = request.FILES['document']
   if request.method == 'POST':
-    uploaded_file = request.FILES['document']
-    print(uploaded_file)
+    fs= FileSystemStorage()
+    fs.save(uploaded_file.name, uploaded_file)
+
+  with open(f'media/{uploaded_file.name}', 'r')as file:
+
+      for value in file:
+        print(value[1])
+
   return render(request,'upload.html')
+
+  #REVISAR SOBRE METODOS DE STRING PARA SETAR TODOS OS ATRIBUTOS DO MEU OBJETO E FAZER MINHA REQUISIÇÃO
