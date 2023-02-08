e97c776005f674ee7fbd9327da120874386d1437
= <r.netofreire45@gmail.com>
2023-02-08 10:37:15 -0300

    cnab em andamento

--- FileCNAB/models.py
+++ FileCNAB/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 
+
 class FileCNAB(models.Model):
-  tipo = models.IntegerField(max_length=1)
+  tipo = models.TextField(max_length=1)
   data = models.TextField(max_length=8)
-  valor = models.IntegerField(max_length=10)
-  CPF = models.IntegerField(max_length=11)
-  cartão = models.IntegerField(max_length=12)
-  hora = models.IntegerField(max_length=6)
+  valor = models.TextField(max_length=10)
+  natureza = models.TextField(max_length=10)
+  CPF = models.TextField(max_length=11)
+  cartao = models.TextField(max_length=12)
+  hora = models.TextField(max_length=6)
   dono = models.TextField(max_length=14)
   loja = models.TextField(max_length=19)
 
--- FileCNAB/views.py
+++ FileCNAB/views.py
@@ -1,17 +1,60 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from FileCNAB.models import FileCNAB
+from django.forms.models import model_to_dict
+
 
 def upload(request):
+  if request.method == 'GET':
+
+        return render(request,'upload.html')
   uploaded_file = request.FILES['document']
   if request.method == 'POST':
     fs= FileSystemStorage()
     fs.save(uploaded_file.name, uploaded_file)
-
+  
   with open(f'media/{uploaded_file.name}', 'r')as file:
-
+      
       for value in file:
-        print(value[1])
+        data = {}
+        if(value[0]=='1'):
+          data['tipo'] = 'Debito'
+          data['natureza'] = 'Entrada'
+        elif(value[0]=='2'):
+          data['tipo'] = 'Boleto'
+          data['natureza'] = 'Saida'
+        elif(value[0]=='3'):
+          data['tipo'] = 'Financiamento'
+          data['natureza'] = 'Saida' 
+        elif(value[0]=='4'):
+          data['tipo'] = 'Credito'
+          data['natureza'] = 'Entrada' 
+        elif(value[0]=='5'):
+          data['tipo'] = 'Recebimento Emprestimo'
+          data['natureza'] = 'Entrada'
+        elif(value[0]=='6'):
+          data['tipo'] = 'Vendas'
+          data['natureza'] = 'Entrada'
+        elif(value[0]=='7'):
+          data['tipo'] = 'Recebimento TED'
+          data['natureza'] = 'Entrada'
+        elif(value[0]=='8'):
+          data['tipo'] = 'Recebimento DOC'
+          data['natureza'] = 'Entrada'
+        elif(value[0]=='9'):
+          data['tipo'] = 'Aluguel'
+          data['natureza'] = 'Saida'
+        
+        data['data'] = value[1:9]
+        data['valor'] = value[9:19]
+        data['CPF'] = value[19:30]
+        data['cartao'] = value[30:42]
+        data['hora'] = value[42:48]
+        data['dono'] = value[48:62]
+        data['loja'] = value[62:81]
+        
+        operation = FileCNAB.objects.create(**data)
 
   return render(request,'upload.html')
 
-  #REVISAR SOBRE METODOS DE STRING PARA SETAR TODOS OS ATRIBUTOS DO MEU OBJETO E FAZER MINHA REQUISIÇÃO
+  
